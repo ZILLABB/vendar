40f465ffcf66152554a8e74f6ba9ab550ab1b1c9
Bayodele Shedu <shedubayode@gmail.com>
2023-08-08 13:51:55 +0100

    interface

--- assets/js/table-datatable.js
+++ assets/js/table-datatable.js
@@ -1,20 +1,16 @@
-$(function() {
-	"use strict";
+$(function () {
+  "use strict";
 
-    $(document).ready(function() {
-        $('#example').DataTable();
-      } );
+  $(document).ready(function () {
+    $("#example").DataTable();
+  });
 
+  $(document).ready(function () {
+    var table = $("#example2").DataTable({
+      lengthChange: false,
+      buttons: ["copy", "csv", "pdf", "print"],
+    });
 
-      $(document).ready(function() {
-        var table = $('#example2').DataTable( {
-            lengthChange: false,
-            buttons: [ 'copy', 'excel', 'pdf', 'print']
-        } );
-     
-        table.buttons().container()
-            .appendTo( '#example2_wrapper .col-md-6:eq(0)' );
-    } );
-
-
+    table.buttons().container().appendTo("#example2_wrapper .col-md-6:eq(0)");
+  });
 });
